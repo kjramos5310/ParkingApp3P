168a4f4359048daf3164478d92ecadf44d846c02
Jesus Ramos <105337663+kjramos5310@users.noreply.github.com>
2026-07-06 16:17:47 -0500

    Ramos - Implementacion de auditoria con RabbitMQ en ms-vehiculos y ms-tickets, correccion de entidades/DTOs en ms-audith, y arreglos de base de datos/conexion

--- App/ms-tickets/src/tickets/tickets.module.ts
+++ App/ms-tickets/src/tickets/tickets.module.ts
@@ -4,13 +4,24 @@ import { TicketsController } from './tickets.controller';
 import { TicketsService } from './tickets.service';
 import { Ticket } from './entities/ticket.entity';
 import { HttpClientService } from './common/httpl-client.service';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { EventPublisher } from '../common/event.publisher.service';
+import { AuditInterceptor } from '../common/interceptors/audit.interceptor';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Ticket]),
   ],
   controllers: [TicketsController],
-  providers: [TicketsService, HttpClientService],
+  providers: [
+    TicketsService,
+    HttpClientService,
+    EventPublisher,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: AuditInterceptor,
+    },
+  ],
   exports: [TicketsService],
 })
 export class TicketsModule { }
